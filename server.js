3f9f74ae2d38e119efd2a818b5116fdadf5896cb
abv313 <helge.johannsen@haw-hamburg.de>
2021-11-09 06:12:35 +0100

    init

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 'use strict';
 
 const express = require('express');
-const { join } = require("path");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const app = express();
-
-app.use(morgan("dev"));
-app.use(helmet());
-app.use(express.static(join(__dirname, "public")));
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-// App
-const app = express();
+const { join } = require("path");
 
 app.get("/auth_config.json", (req, res) => {
-    res.sendFile(join(__dirname, "auth_config.json"));
+  res.sendFile(join(__dirname, "auth_config.json"));
 });
 
 app.get("/*", (_, res) => {
-    res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "index.html"));
 });
 
 process.on("SIGINT", function() {
-    process.exit();
+  process.exit();
 });
 
 app.listen(PORT, HOST);
